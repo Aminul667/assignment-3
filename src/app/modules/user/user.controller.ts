a95861e9fe001e945057f2f59813f11132b096dd
Aminul667 <rahathssn@gmail.com>
2024-06-20 17:33:37 +0600

    added create user controller

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,20 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+
+const createUser = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body;
+
+  const result = await UserServices.createStudentIntoDB(password, studentData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  createUser,
+};
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserSignupValidation } from './user.validation';
+import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
 router.post(
   '/auth/signup',
   validateRequest(UserSignupValidation.userSignupValidationSchema),
-  UserControllers.createStudent,
+  UserControllers.createUser,
 );
