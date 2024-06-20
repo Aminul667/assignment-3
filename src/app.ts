ade547e3ea23b6c9cf37329f5673cc097865c2aa
Aminul667 <rahathssn@gmail.com>
2024-06-20 16:43:14 +0600

    added middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // import cookieParser from 'cookie-parser';
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import router from "./app/routes";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 // import globalErrorHandler from './app/middlewares/globalErrorhandler';
 // import notFound from './app/middlewares/notFound';
 // import router from './app/routes';
@@ -15,21 +17,20 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 // app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application routes
 // app.use("/api", router);
 
-const test = async (req: Request, res: Response) => {
-  res.send("App is running");
-};
+// const test = async (req: Request, res: Response) => {
+//   res.send('App is running');
+// };
 
-// app.get("/", test);
-app.get("/", test);
+// app.get('/', test);
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 //Not Found
-// app.use(notFound);
+app.use(notFound);
 
 export default app;
