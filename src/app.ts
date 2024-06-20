550565f416372e0ab8a62884438292aafe74c6bf
Aminul667 <rahathssn@gmail.com>
2024-06-20 16:35:45 +0600

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import cookieParser from 'cookie-parser';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
+// import globalErrorHandler from './app/middlewares/globalErrorhandler';
+// import notFound from './app/middlewares/notFound';
+// import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+// app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5173"] }));
+
+// application routes
+// app.use("/api", router);
+
+const test = async (req: Request, res: Response) => {
+  res.send("App is running");
+};
+
+// app.get("/", test);
+app.get("/", test);
+
+// app.use(globalErrorHandler);
+
+//Not Found
+// app.use(notFound);
+
+export default app;
